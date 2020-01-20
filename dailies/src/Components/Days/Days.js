d6d2d1a02536451fbb5bc10a1e471c5acb1d10f6
bonniepeters <bonnie.e.peters@gmail.com>
2020-01-20 16:09:29 -0700

    Images Added

--- dailies/src/Components/Days/Days.js
+++ dailies/src/Components/Days/Days.js
@@ -6,27 +6,30 @@ class Days extends Component {
     const days = data.map(day => {
       return (
         <>
+          {/*  */}
+          <h2>{day.numbered}</h2>
           <ul>
-            <li>{day.caption}</li>
-            <li>{day.numbered}</li>
-            <li>{day.day}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/water.png'}/></li>
+            <li><img src={process.env.PUBLIC_URL + '/images/work.png'}/></li>
+            <li><img src={process.env.PUBLIC_URL + '/images/connection.png'}/></li>
+            <li><img src={process.env.PUBLIC_URL + '/images/workout.png'}/></li>
+            <li><img src={process.env.PUBLIC_URL + '/images/music.png'}/></li>
+            <li><img src={process.env.PUBLIC_URL + '/images/weight.png'}/></li>
+            <li>
+              <p>{day.caption}</p>
+            </li>
+            <li><img src={process.env.PUBLIC_URL + '/images/code.png'}/>{day.code}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/oils.png'}/>{day.oils}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/mindful.png'}/>{day.mindful}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/food.png'}/>{day.food}</li>
             <li>{day.week}</li>
-            <li>{day.water}</li>
-            <li>{day.work}</li>
-            <li>{day.connection}</li>
-            <li>{day.workout}</li>
-            <li>{day.music}</li>
-            <li>{day.code}</li>
-            <li>{day.mindful}</li>
-            <li>{day.read}</li>
-            <li>{day.learn}</li>
-            <li>{day.vitamins}</li>
-            <li>{day.timeSlots}</li>
-            <li>{day.outside}</li>
-            <li>{day.earth}</li>
-            <li>{day.food}</li>
-            <li>{day.oils}</li>
-            <li>{day.weight}</li>
+            <li>{day.day}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/read.png'}/>{day.read}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/earth.png'}/>{day.earth}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/outside.png'}/>{day.outside}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/timeSlots.png'}/>{day.timeSlots}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/vitamins.png'}/>{day.vitamins}</li>
+            <li><img src={process.env.PUBLIC_URL + '/images/learn.png'}/>{day.learn}</li>
             <li>{day.awake}</li>
             <li>{day.asleep}</li>
           </ul>
