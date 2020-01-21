0c4e4c4a7b25a4f3790f77df35c9d489b355c2a4
bonniepeters <bonnie.e.peters@gmail.com>
2020-01-21 11:44:26 -0700

    responsive nav

--- dailies/src/Components/Days/Days.js
+++ dailies/src/Components/Days/Days.js
@@ -5,34 +5,38 @@ class Days extends Component {
   render() {
     const days = data.map(day => {
       return (
-        <>
-          <h2 class="day-number">{day.numbered}</h2>
-          <ul class="outer-box">
-            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/water.png'}/></li>
-            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/work.png'}/></li>
-            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/connection.png'}/></li>
-            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/workout.png'}/></li>
-            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/music.png'}/></li>
-            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/weight.png'}/>{day.weight}</li>
-            <li>
-              <p class="caption">{day.caption}</p>
+        <section>
+          <h2 className="day-number">{day.numbered}</h2>
+          <ul className="wrapper">
+            <li className="icon" ><img className="icon-img" src={process.env.PUBLIC_URL + '/images/water.png'}/></li>
+            <li className="icon" ><img className="icon-img" src={process.env.PUBLIC_URL + '/images/work.png'}/></li>
+            <li className="icon" ><img className="icon-img" src={process.env.PUBLIC_URL + '/images/connection.png'}/></li>
+            <li className="icon" ><img className="icon-img" src={process.env.PUBLIC_URL + '/images/workout.png'}/></li>
+            <li className="icon" ><img className="icon-img" src={process.env.PUBLIC_URL + '/images/music.png'}/></li>
+            <li className="icon" ><img className="icon-img" src={process.env.PUBLIC_URL + '/images/weight.png'} />
+              {/* {day.weight} */}
             </li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/code.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/oils.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/mindful.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/food.png'}/></li>
-            <li class="corner-text" >{day.week}</li>
-            <li class="corner-text" >{day.day}</li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/read.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/earth.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/outside.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/timeSlots.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/vitamins.png'}/></li>
-            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/learn.png'}/></li>
-            <li class="corner-text" >{day.awake}</li>
-            <li class="corner-text" >{day.asleep}</li>
+            <li className="inner-box">
+              <p className="caption">{day.caption}</p>
+              <p className="day-week" >{day.week}</p>
+              <p className="week-number" >{day.day}</p>
+            </li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/code.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/oils.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/mindful.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/food.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/read.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/earth.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/outside.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/timeSlots.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/vitamins.png'}/></li>
+            <li className="icon" ><img src={process.env.PUBLIC_URL + '/images/learn.png'}/></li>
           </ul>
-        </>
+          <div className="sleep">
+            <p className="awake" >{day.awake}</p>
+            <p className="asleep" >{day.asleep}</p>
+          </div>
+        </section>
       );
     });
     return <div className="day">{days}</div>;
--- dailies/src/Components/Nav/Nav.js
+++ dailies/src/Components/Nav/Nav.js
@@ -3,8 +3,17 @@ import React, { Component } from "react";
 class Nav extends Component {
   render() {
     return (
-      <nav className="nav">
-      </nav>
+      <header className="header">
+        <a href="/" className="logo">Practically Imperfect</a>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" for="menu-btn"><span className="nav-icon"></span></label>
+        <ul className="menu">
+          <li><a href="#dont">Don't</a></li>
+          <li><a href="#forget">Forget</a></li>
+          <li><a href="#subscribe">To Subscribe</a></li>
+          <li><a href="#comment">& Comment</a></li>
+        </ul>
+      </header>
     );
   }
 }
