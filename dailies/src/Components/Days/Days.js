35ce8dbcef33559860e5d26b3e1ba8c8b052a693
bonniepeters <bonnie.e.peters@gmail.com>
2020-01-21 08:59:55 -0700

    first stages of grid

--- dailies/src/Components/Days/Days.js
+++ dailies/src/Components/Days/Days.js
@@ -6,32 +6,31 @@ class Days extends Component {
     const days = data.map(day => {
       return (
         <>
-          {/*  */}
-          <h2>{day.numbered}</h2>
-          <ul>
-            <li><img src={process.env.PUBLIC_URL + '/images/water.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/work.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/connection.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/workout.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/music.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/weight.png'}/>{day.weight}</li>
+          <h2 class="day-number">{day.numbered}</h2>
+          <ul class="outer-box">
+            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/water.png'}/></li>
+            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/work.png'}/></li>
+            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/connection.png'}/></li>
+            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/workout.png'}/></li>
+            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/music.png'}/></li>
+            <li class="icon" ><img class="icon-img" src={process.env.PUBLIC_URL + '/images/weight.png'}/>{day.weight}</li>
             <li>
-              <p>{day.caption}</p>
+              <p class="caption">{day.caption}</p>
             </li>
-            <li><img src={process.env.PUBLIC_URL + '/images/code.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/oils.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/mindful.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/food.png'}/></li>
-            <li>{day.week}</li>
-            <li>{day.day}</li>
-            <li><img src={process.env.PUBLIC_URL + '/images/read.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/earth.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/outside.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/timeSlots.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/vitamins.png'}/></li>
-            <li><img src={process.env.PUBLIC_URL + '/images/learn.png'}/></li>
-            <li>{day.awake}</li>
-            <li>{day.asleep}</li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/code.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/oils.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/mindful.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/food.png'}/></li>
+            <li class="corner-text" >{day.week}</li>
+            <li class="corner-text" >{day.day}</li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/read.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/earth.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/outside.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/timeSlots.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/vitamins.png'}/></li>
+            <li class="icon" ><img src={process.env.PUBLIC_URL + '/images/learn.png'}/></li>
+            <li class="corner-text" >{day.awake}</li>
+            <li class="corner-text" >{day.asleep}</li>
           </ul>
         </>
       );
